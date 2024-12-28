90c789ca8c3748cc8754da7316aef81e9387e6f2
faisalalbarq <faisalalbarq09@gmail.com>
2024-12-28 21:45:45 +0300

    Add The Employee Module

--- LinkDev.IKEA.DataAccessLayer/Persistence/Data/Migrations/20241228184219_EmployeeModuleMigration.Designer.cs
+++ LinkDev.IKEA.DataAccessLayer/Persistence/Data/Migrations/20241228184219_EmployeeModuleMigration.Designer.cs
@@ -4,6 +4,7 @@ using LinkDev.IKEA.DataAccessLayer.Persistence.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LinkDev.IKEA.DataAccessLayer.Persistence.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241228184219_EmployeeModuleMigration")]
+    partial class EmployeeModuleMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
